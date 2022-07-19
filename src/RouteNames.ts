2e740a0de5f62f38cd121d9b4a2a9665b8193999
LeeSujeong329 <olivia734267@gmail.com>
2022-07-19 23:05:35 +0900

    RouteNames 추가

---
+++ src/RouteNames.ts
@@ -0,0 +1,10 @@
+export const ROUTE_NAMES = {
+  MAIN_SCREEN: 'MainScreen',
+  FIND_ACCOUNT_SCREEN: 'FindAccountScreen',
+  SIGN_UP_SCREEN: 'SignUpScreen',
+  MY_INFO_SCREEN: 'MyInfoScreen',
+  QR_SCAN_SCREEN: 'QRScanScreen',
+  QR_CODE_INPUT_SCREEN: 'QRCodeInputScreen',
+  BUILDING_MANAGEMENT_SCREEN: 'BuildingManagementScreen',
+  RESIDENT_MANAGEMENT_SCREEN: 'ResidentManagementScreen',
+}
