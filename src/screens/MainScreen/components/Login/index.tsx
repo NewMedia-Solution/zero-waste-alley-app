7075161b2e69286bf09df7ba56200b0c98df4ca0
LeeSujeong329 <olivia734267@gmail.com>
2022-09-06 23:54:24 +0900

    로그인 컴포넌트 추가

--- src/screens/MainScreen/components/Login/index.tsx
+++ src/screens/MainScreen/components/Login/index.tsx
@@ -1,6 +1,165 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState } from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import logo from '../../../../../assets/images/logo.png'
+import { Colors, FontFamily } from '../../../../Constants'
+import { useDispatch } from 'react-redux'
+import { NavigationiService } from '../../../../services/NavigationiService'
+import { ROUTE_NAMES } from '../../../../RouteNames'
 
 export const LoginComponent = () => {
-  return <View></View>
+  const dispatch = useDispatch()
+
+  const [id, setId] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onPressLoginButton = () => {}
+
+  const onPressFindIdButton = () => {
+    NavigationiService.navigate(ROUTE_NAMES.FIND_ACCOUNT_SCREEN, {
+      via: 'findId',
+    })
+  }
+
+  const onPressFindPasswordButton = () => {
+    NavigationiService.navigate(ROUTE_NAMES.FIND_ACCOUNT_SCREEN, {
+      via: 'findPassword',
+    })
+  }
+
+  const onPressSignUpButton = () => {
+    NavigationiService.navigate(ROUTE_NAMES.SIGN_UP_SCREEN)
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Image style={styles.logo} source={logo} />
+      </View>
+      <View style={styles.bottomContainer}>
+        <TextInput
+          style={styles.id}
+          placeholder={'아이디'}
+          placeholderTextColor={Colors.gray2}
+          onChangeText={setId}
+        />
+        <TextInput
+          style={styles.password}
+          placeholder={'비밀번호'}
+          placeholderTextColor={Colors.gray2}
+          onChangeText={setPassword}
+        />
+        <TouchableHighlight
+          style={styles.loginButton}
+          onPress={onPressLoginButton}
+          underlayColor={Colors.green2}
+        >
+          <Text style={styles.loginButtonText}>LOGIN</Text>
+        </TouchableHighlight>
+        <View style={styles.bottomButtonContainer}>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            onPress={onPressFindIdButton}
+            activeOpacity={0.7}
+          >
+            <Text style={styles.bottomButtonText}>아이디 찾기</Text>
+          </TouchableOpacity>
+          <Text style={styles.bottomButtonText}>|</Text>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            onPress={onPressFindPasswordButton}
+            activeOpacity={0.7}
+          >
+            <Text style={styles.bottomButtonText}>비밀번호 찾기</Text>
+          </TouchableOpacity>
+          <Text style={styles.bottomButtonText}>|</Text>
+          <TouchableOpacity
+            style={styles.bottomButton}
+            onPress={onPressSignUpButton}
+            activeOpacity={0.7}
+          >
+            <Text style={styles.bottomButtonText}>회원가입</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 40,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: Colors.white,
+  },
+  topContainer: {
+    width: '100%',
+  },
+  logo: {
+    width: '100%',
+    height: 280,
+    resizeMode: 'contain',
+  },
+  bottomContainer: {
+    width: '100%',
+  },
+  id: {
+    width: '100%',
+    fontFamily: FontFamily.medium,
+    fontSize: 14,
+    color: Colors.black,
+    paddingHorizontal: 10,
+    borderBottomWidth: 2,
+    borderColor: Colors.gray2,
+    borderRadius: 10,
+  },
+  password: {
+    width: '100%',
+    fontFamily: FontFamily.medium,
+    fontSize: 14,
+    color: Colors.black,
+    marginTop: 20,
+    paddingHorizontal: 10,
+    borderBottomWidth: 2,
+    borderColor: Colors.gray2,
+    borderRadius: 10,
+  },
+  loginButton: {
+    width: '100%',
+    paddingVertical: 15,
+    marginTop: 35,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    backgroundColor: Colors.green,
+  },
+  loginButtonText: {
+    fontFamily: FontFamily.bold,
+    fontSize: 14,
+    letterSpacing: 2,
+    color: Colors.white,
+  },
+  bottomButtonContainer: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    marginTop: 15,
+    marginBottom: 30,
+  },
+  bottomButton: {
+    marginHorizontal: 10,
+  },
+  bottomButtonText: {
+    fontFamily: FontFamily.medium,
+    fontSize: 14,
+    color: Colors.gray3,
+  },
+})
